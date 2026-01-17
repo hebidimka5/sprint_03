bd1b7dd49102d1786065e5bbd409bb4992d85dc7
Nikolay <potsy02@Mail.ru>
2026-01-18 00:13:41 +0800

    Выполнено задание

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var historyTextView: UITextView!
+    @IBOutlet weak var reButton: UIButton!
+    @IBOutlet weak var decreaceButton: UIButton!
+    private var counter: Int = 0
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterLabel.text = "Значение счётчика: \(counter)"
+        
+        historyTextView.text = "История изменений:"
+        
+        decreaceButton.tintColor = .green
+        changeButton.tintColor = .red
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+        addHistoryRecord("Значение изменено на +1")
+    }
+    
+    @IBAction func buttonRestart(_ sender: Any) {
+        counter = 0
+        counterLabel.text = "Значение счётчика: \(counter)"
+        addHistoryRecord("значение сброшено")
+    }
+    
+    @IBAction func buttonDecreased(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            counterLabel.text = "Значение счётчика: \(counter)"
+            addHistoryRecord("Значение изменено на -1")
+        } else {
+            addHistoryRecord("попытка уменьшить значение счётчика ниже 0")
+            
+        }
+    }
+    
+    func currentDateString() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        return formatter.string(from: Date())
+    }
+    
+    func addHistoryRecord(_ text: String) {
+        let record = "[\(currentDateString())]: \(text)"
+        historyTextView.text += "\n" + record
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,14 @@
+On branch sprint_03
+Your branch is up to date with 'origin/sprint_03'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	modified:   Counter/Base.lproj/Main.storyboard
+	modified:   Counter/ViewController.swift
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	README.md
+
+no changes added to commit (use "git add" and/or "git commit -a")
